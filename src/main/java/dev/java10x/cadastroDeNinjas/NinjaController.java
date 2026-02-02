20dac40a838472eba3fecc789c26c8cb76d95ad2
Harakk <rtsph9999@gmail.com>
2026-02-01 21:52:35 -0300

    second commit

---
+++ src/main/java/dev/java10x/cadastroDeNinjas/NinjaController.java
@@ -0,0 +1,20 @@
+package dev.java10x.cadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bemVindo")
+public class NinjaController {
+
+    @GetMapping("/positivo")
+    public String boasVindas(){
+        return "ola";
+    }
+
+    @GetMapping("/negativo")
+    public String pessimasVindas(){
+        return "Vai embora";
+    }
+}
